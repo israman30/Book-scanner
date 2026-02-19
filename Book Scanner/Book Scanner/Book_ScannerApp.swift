ad8c100ce0f1299738ec41c88a699c5555cc2301
Israel Manzo <israelmanzo814@gmail.com>
2026-02-19 13:46:20 -0500

    added new xcode project

---
+++ Book Scanner/Book Scanner/Book_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_ScannerApp.swift
+//  Book Scanner
+//
+//  Created by Israel Manzo on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Book_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

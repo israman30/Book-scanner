61149bf3a82203025c8b74446516a883210a5b6e
Israel Manzo <israelmanzo814@gmail.com>
2026-02-21 22:28:02 -0500

    added unit test

---
+++ Book Scanner/Book ScannerTests/Book_ScannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Book_ScannerTests.swift
+//  Book ScannerTests
+//
+//  Created by Israel Manzo on 2/21/26.
+//
+
+import Testing
+
+struct Book_ScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
